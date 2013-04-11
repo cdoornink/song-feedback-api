ae984ec2c118bcde6aa3dc3e423f48104ed65729
cdoornink <cdoornink@rhapsody.com>
2013-04-11 16:01:02 -0700

    adding some logging to songs routes

--- routes/songs.js
+++ routes/songs.js
@@ -4,9 +4,9 @@ exports.find = function(req, res) {
   db.collection('songs', function(err, collection) {
     if(err){console.log('ERROR 1.0.0 :: '+err)}else{console.log("retrieved songs collection")}
     console.log(collection[0]);
-    collection.find({'_id': id},{name:1, artist:1}, function(err, item) {
-      if(err){console.log('ERROR 1.0.1 :: '+err)}else{console.log("found song item: "+item);console.log(item)}
-      res.send(item);
+    collection.find({_id: id}).toArray(function(err, results){
+      if(err){console.log('ERROR 1.0.1 :: '+err)}else{console.log("found song item: "+results)}
+      res.send(results);
     });
   });
 };
